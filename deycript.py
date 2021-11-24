b33a0becd732b5fca8d2d7e89108a74a687bc11a
piter <68443246+PiterIvano@users.noreply.github.com>
2021-11-24 18:47:04 +0100

    1.2

--- deycript.py
+++ deycript.py
@@ -4,13 +4,14 @@ import os
 import json
 
 os.system('color 2')
-Ver = platform.system()
-if Ver == "Linux":
-    os.system("clear")
-elif Ver == "Windows":
-    os.system("cls")
-else:
-    print("[+] No se reconoce el sistema operativo")
+def clear():
+    Ver = platform.system()
+    if Ver == "Linux":
+        os.system("clear")
+    elif Ver == "Windows":
+        os.system("cls")
+    else:
+        print("[+] No se reconoce el sistema operativo")
     
 while True:
     def history():
@@ -18,40 +19,55 @@ while True:
         print("[+] 2 Ver descargas")
         ver = input("[+] Seleccione una opcion-> ")
         if ver == "1":
-            print("El archivo debe llamarse History.db")
-            conn = sqlite3.connect('History.db')
-            c = conn.cursor()
-            c.execute('SELECT url, title FROM urls ORDER BY last_visit_time DESC')
-            for row in c.fetchall():
-                json_data = json.dumps(row)
-                json_data = json.loads(json_data)
-                print(f"[+] URL: {json_data[0]} \n[+] Nombre: {json_data[1]}\n")
+            try:
+                print("El archivo debe llamarse History.db")
+                conn = sqlite3.connect('History.db')
+                c = conn.cursor()
+                c.execute('SELECT url, title FROM urls ORDER BY last_visit_time DESC')
+                for row in c.fetchall():
+                    json_data = json.dumps(row)
+                    json_data = json.loads(json_data)
+                    print(f"[+] URL: {json_data[0]} \n[+] Nombre: {json_data[1]}\n")
+                conn.close()
+            except:
+                clear()
+                print("[+] No se pudo abrir el archivo")
                 
                 
                 
         elif ver == "2":
-            print("El archivo debe llamarse History.db")
-            conn = sqlite3.connect('History.db')
-            c = conn.cursor()
-            c.execute('SELECT id, current_path FROM downloads')
-            for row in c.fetchall():
-                json_data = json.dumps(row)
-                json_data = json.loads(json_data)
-                print(f"[+] ID: {json_data[0]} \n[+] Descarga: {json_data[1]}\n")
+            try:
+                print("El archivo debe llamarse History.db")
+                conn = sqlite3.connect('History.db')
+                c = conn.cursor()
+                c.execute('SELECT id, current_path FROM downloads')
+                for row in c.fetchall():
+                    json_data = json.dumps(row)
+                    json_data = json.loads(json_data)
+                    print(f"[+] ID: {json_data[0]} \n[+] Descarga: {json_data[1]}\n")
+                conn.close()
+            except:
+                clear()
+                print("[+] No se encontro el archivo")
                 
     def cookies():
-        print("[+] Alert: El nombre debe ser Cookies.db")
-        print("[+] Escriba 1 para iniciar")
-        ver = input("[+] Seleccione una opcion-> ")
-        if ver == "1":
-            print("El archivo debe llamarse Cookies.db")
-            conn = sqlite3.connect('Cookies.db')
-            c = conn.cursor()
-            c.execute('SELECT host_key, name, encrypted_value FROM cookies')
-            for row in c.fetchall():
-                print(row)
-        elif ver != "2":
-            print("[+] Opcion no valida")
+        try:
+            print("[+] Alert: El nombre debe ser Cookies.db")
+            print("[+] Escriba 1 para iniciar")
+            ver = input("[+] Seleccione una opcion-> ")
+            if ver == "1":
+                print("El archivo debe llamarse Cookies.db")
+                conn = sqlite3.connect('Cookies.db')
+                c = conn.cursor()
+                c.execute('SELECT host_key, name, encrypted_value FROM cookies')
+                for row in c.fetchall():
+                    print(row)
+                conn.close()
+            else:
+                print("[+] No se pudo abrir el archivo")
+        except:
+            clear()
+            print("[+] No se pudo abrir el archivo, revise el nombre")
                 
                 
     def passw():
@@ -59,42 +75,57 @@ while True:
         print("[+] Escriba 1 para iniciar")
         ver = input("[+] Seleccione una opcion-> ")
         if ver == "1":
-            print("El archivo debe llamarse Login Data.db")
-            conn = sqlite3.connect('Login Data.db')
-            c = conn.cursor()
-            c.execute('SELECT origin_url, username_value, password_value, date_password_modified FROM logins')
-            for row in c.fetchall():
-                """print(row)"""
-                url = row[0]
-                username = row[1]
-                passww = row[2]
-                date = row[3]
-                print(f"[+] URL: {url} \n[+] Usuario: {username} \n[+] Contraseña encriptada: {passww} \n[+] Date_pass: {date}")
-                print("\n")
+            try:
+                print("El archivo debe llamarse Login Data.db")
+                conn = sqlite3.connect('Login Data.db')
+                c = conn.cursor()
+                c.execute('SELECT origin_url, username_value, password_value, date_password_modified FROM logins')
+                for row in c.fetchall():
+                    """print(row)"""
+                    url = row[0]
+                    username = row[1]
+                    passww = row[2]
+                    date = row[3]
+                    print(f"[+] URL: {url} \n[+] Usuario: {username} \n[+] Contraseña encriptada: {passww} \n[+] Date_pass: {date}")
+                    print("\n")
+                conn.close()
+            except:
+                clear()
+                print("[+] No se pudo abrir el archivo")
+                
                 
     def web_data(): 
-        print("[+] Alert: El nombre debe ser Web Data.db")
-        print("[+] Escriba 1 autofill")
-        print("[+] Escriba 2 tarjetas de credito")
-        ver = input("[+] Seleccione una opcion-> ")
-        if ver == "1":
-            print("El archivo debe llamarse Web Data.db")
-            conn = sqlite3.connect('Web Data.db')
-            c = conn.cursor()
-            c.execute('SELECT name, value FROM autofill')
-            for row in c.fetchall():
-                json_data = json.dumps(row)
-                json_data = json.loads(json_data)
-                print(f"\n[+] Nombre: {json_data[0]} \n[+] Valor: {json_data[1]}")
-        elif ver == "2":
-            print("El archivo debe llamarse Web Data.db")
-            conn = sqlite3.connect('Web Data.db')
-            c = conn.cursor()
-            c.execute('SELECT status, name_on_card, network, last_four, exp_month, exp_year FROM masked_credit_cards')
-            for row in c.fetchall():
-                json_data = json.dumps(row)
-                json_data = json.loads(json_data)
-                print(f"\n[+] Estado: {json_data[0]} \n[+] Nombre: {json_data[1]} \n[+] Tipo: {json_data[2]} \n[+] Ultimos Dijitos: {json_data[3]} \n[+]Expira mes: {json_data[4]} \n[+] Expira año: {json_data[5]}")
+        try:
+            print("[+] Alert: El nombre debe ser Web Data.db")
+            print("[+] Escriba 1 autofill")
+            print("[+] Escriba 2 tarjetas de credito")
+            ver = input("[+] Seleccione una opcion-> ")
+            if ver == "1":
+                print("El archivo debe llamarse Web Data.db")
+                conn = sqlite3.connect('Web Data.db')
+                c = conn.cursor()
+                c.execute('SELECT name, value FROM autofill')
+                for row in c.fetchall():
+                    json_data = json.dumps(row)
+                    json_data = json.loads(json_data)
+                    print(f"\n[+] Nombre: {json_data[0]} \n[+] Valor: {json_data[1]}")
+            elif ver == "2":
+                print("El archivo debe llamarse Web Data.db")
+                conn = sqlite3.connect('Web Data.db')
+                c = conn.cursor()
+                c.execute('SELECT status, name_on_card, network, last_four, exp_month, exp_year FROM masked_credit_cards')
+                for row in c.fetchall():
+                    json_data = json.dumps(row)
+                    json_data = json.loads(json_data)
+                    print(f"\n[+] Estado: {json_data[0]} \n[+] Nombre: {json_data[1]} \n[+] Tipo: {json_data[2]} \n[+] Ultimos Dijitos: {json_data[3]} \n[+]Expira mes: {json_data[4]} \n[+] Expira año: {json_data[5]}")
+                conn.close()
+            else:
+                print("[+] No se pudo abrir el archivo")
+        except:
+            clear()
+            print("[+] no hay tarjetas incluidas")
+            
+    
     print("___________________________________________________________________________________")
     print("[+] 1 Ver historial")
     print("[+] 2 Ver cookies(Mantenimiento)")
